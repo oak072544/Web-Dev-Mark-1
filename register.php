ba50d07fcf0daaa6092b903b49d4ed0518e6b020
oak072544 <oak072544@gmail.com>
2022-10-26 12:19:41 +0700

    work6&7

--- register.php
+++ register.php
@@ -12,23 +12,81 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment_2[register]</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>Webboard Mark 1[register]</title>
 </head>
 <body>
-    <h1 style="text-align: center;">สมัครสมาชิค</h1>
-    <hr>
-    <table style="border: 2px solid black; width: 40%;" align="center">
-        <tr style="background-color: #6CD2FE;"><td colspan="2">กรอกข้อมูล</td></tr>
-        <tr><td>ชื่อบัญชี : </td><td><input type="text" name="username" size="20"></td></tr>
-        <tr><td>รหัสผ่าน : </td><td><input type="password" name="pwd" size="20"><br></td></tr>
-        <tr><td>ชื่อ-นามสกุล : </td><td><input type="text" name="name-surname" size="20"></td></tr>
-        <tr><td>เพศ : </td><td><input type="radio" name="gender" id="male">ชาย <br>
-                                <input type="radio" name="gender" id="female">หญิง <br>
-                                <input type="radio" name="gender" id="other">อื่นๆ </td></tr>
-        <tr><td>อีเมลล์ : </td><td><input type="text" name="email" size="20"></td></tr>
-        <tr><td colspan="2" align="center"><input type="submit" value="สมัครสมาชิค"></td></tr>
-    </table>
+    <div class=" container">
+    <h1 style="text-align: center;">Webboard Mark 1</h1>
+    <?php include "nav.php" ; ?>
     <br>
-    <div style="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></div>
+    <div class=" row">
+        <div class=" col-md-3"></div>
+        <div class=" col-md-6">
+            <div class=" card text-dark bg-white border-primary">
+                <div class=" card-header bg-primary text-white">
+                    กรอกข้อมูล
+                </div>
+                <div class=" card-body">
+                    <form action="register_save.php" method="POST">
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">ชื่อบัญชี : </label>
+                            <div class="col-md-9">
+                                <input type="text" name="login" class=" form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">รหัสผ่าน : </label>
+                            <div class="col-md-9">
+                                <input type="password" name="pwd" class=" form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">ชื่อ-นามสกุล : </label>
+                            <div class="col-md-9">
+                                <input type="text" name="name" class=" form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label pt-0">เพศ : </label>
+                            <div class="col-md-9">
+                                <div class=" form-check">
+                                    <input type="radio" name="gender" value="m" class=" form-check-input" required>
+                                    <label class=" form-check-label">ชาย</label>
+                                </div>
+                                <div class=" form-check">
+                                    <input type="radio" name="gender" value="f" class=" form-check-input" required>
+                                    <label class=" form-check-label">หญิง</label>
+                                </div>
+                                <div class=" form-check">
+                                    <input type="radio" name="gender" value="o" class=" form-check-input" required>
+                                    <label class=" form-check-label">อื่นๆ</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">อีเมล : </label>
+                            <div class="col-md-9">
+                                <input type="text" name="email" class=" form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label"></label>
+                            <div class="col-md-9">
+                                <button type="submit" class="btn btn-primary btn-sm"><i class="bi bi-save me-1"></i> สมัครสมาชิค</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class=" col-md-3"></div>
+    </div>
+    
+    </div>
 </body>
 </html>
